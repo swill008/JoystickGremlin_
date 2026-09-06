db4cadae1aca5614e5da77a29a5e5bf052f676c9
swill008 <153114082+swill008@users.noreply.github.com>
2026-09-06 14:43:09 -0500

    Remove misspelled tools_osctools_osc/osc_send_test.py

--- tools_osctools_osc/osc_send_test.py
+++
@@ -1,6 +0,0 @@
-from pythonosc.udp_client import SimpleUDPClient
-
-client = SimpleUDPClient("127.0.0.1", 9000)
-client.send_message("/streamdeck/1", 1)
-client.send_message("/streamdeck/1", 0)
-print("Sent /streamdeck/1 1 then 0 to 127.0.0.1:8000")
